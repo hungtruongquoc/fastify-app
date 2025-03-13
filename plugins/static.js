8f6a288fc4b480a626416d3af8525f7130d304f9
Hung Truong <hungtruongquoc@gmail.com>
2025-03-12 22:21:37 -0500

    Added default 404 page

--- plugins/static.js
+++ plugins/static.js
@@ -8,4 +8,9 @@ module.exports = fp(async function (fastify, opts) {
     fastify.register(fastifyStatic, {
         root: path.join(__dirname, '../public'), // Adjust the path as needed
     })
+
+    // Set 404 handler to serve custom 404.html
+    fastify.setNotFoundHandler((request, reply) => {
+        reply.status(404).sendFile('404.html')
+    })
 })
