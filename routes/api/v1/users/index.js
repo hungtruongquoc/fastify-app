e7cbdcdddeaf0b118ce302321a213cd4da17d3cf
Hung Truong <hungtruongquoc@gmail.com>
2025-03-13 22:58:29 -0500

    Added exception handling

--- routes/api/v1/users/index.js
+++ routes/api/v1/users/index.js
@@ -7,12 +7,17 @@ module.exports = async (fastify, opts) => {
 
     fastify.get('/:id', async (request, reply) => {
         const {id} = request.params;
-        const user = fastify.db.prepare(`SELECT *
+        try {
+            const user = fastify.db.prepare(`SELECT *
                                           FROM users WHERE id = ?`).get(id);
-        if (!user) {
-            throw fastify.httpErrors.notFound()
-        }
+            if (!user) {
+                throw fastify.httpErrors.notFound()
+            }
 
-        return {data: user};
+            return {data: user};
+        }
+        catch (error) {
+            throw fastify.httpErrors.internalServerError("Failed to get user");
+        }
     })
 }
