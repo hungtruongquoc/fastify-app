0075b1ef5ca353271bca560ed4145609f8d76fee
Hung Truong <hungtruongquoc@gmail.com>
2025-03-13 22:40:38 -0500

    Added an endpoint to get detail of a user

--- routes/api/v1/users/index.js
+++ routes/api/v1/users/index.js
@@ -4,4 +4,15 @@ module.exports = async (fastify, opts) => {
                                           FROM users`).all();
         return {data: users};
     })
+
+    fastify.get('/:id', async (request, reply) => {
+        const {id} = request.params;
+        const user = fastify.db.prepare(`SELECT *
+                                          FROM users WHERE id = ?`).get(id);
+        if (!user) {
+            throw fastify.httpErrors.notFound()
+        }
+
+        return {data: user};
+    })
 }
